b78b05cd90e0674b5013a2a31da4dd7f3b825eaa
MingYao <96293549+Mingyao8@users.noreply.github.com>
2025-04-28 12:13:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,6 @@ def list_files():
 @app.route('/uploads/<filename>')
 def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
